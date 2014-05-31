369aee3f4f0a65012dd617962409053a0f4b709d
Vasiliy Horbachenko <shadowprince47@gmail.com>
2014-05-31 12:29:43 +0300

    smallfix

--- seasondog/watcher.py
+++ seasondog/watcher.py
@@ -47,7 +47,7 @@ def watch(runtime, data):
         else:
                 args = player_args(runtime, data, runtime[r.PLAYER_ARGS_OVERRIDE])
 
-        print(r.format("{c_title}Episode {c_ep_number}{bold}#{episode}{endc}\n        {c_path}{path}\n        {c_args}{args}\n{endc}{c_control}[[enter] - watch, [^c] - break]", 
+        print(r.format("{c_title}Episode {c_ep_number}{bold}#{episode}{endc}\n        {c_path}{path}\n        {c_args}{args}\n{endc}{c_control}[[enter] - watch, [^c] - break]{endc}", 
             episode=episode, 
             path=os.path.split(file)[-1],
             args=args,
